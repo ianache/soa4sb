ffe5dd3fafdb966731ff552245be4f1108d0691a
ianache <ilver.anache@gmail.com>
2015-09-26 00:04:53 -0500

    Registro Inicial

---
+++ soa4sb-db/src/main/resources/db/migration/V1__create_service_table.sql
@@ -0,0 +1,6 @@
+-- servicio = { codigo, nombre (v), descripcion (v),  }
+create table SERVICIO (
+	CODIGO VARCHAR(10) NOT NULL,
+	NOMBRE VARCHAR(120) NOT NULL,
+	DESCRIPCION VARCHAR(400) NOT NULL
+);
